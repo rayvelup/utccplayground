d4cfce74088df498914284b02c5917af4bb205c9
unknown <ravenewriter@gmail.com>
2025-09-26 22:24:35 +0700

    update

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
 import pandas as pd
-import streamlit as st 
+import streamlit as st
 
-data = pd.read_csv("product.csv")
+st.title("Simple CSV Viewer")
 
+# File uploader
+upload_file = st.file_uploader("Choose a CSV file", type="csv")
 
-st.title("dashboard")
-st.write("this application is about displaying product sales")
-
-if 'username' in st.session_state:
-    st.write("hello!"(st.session_state['Username']))
-else: 
-    st.info("please register something")
-
-
-Upload_file = st.file_uploader("choose a csvfile",type="csv")
-
-if Upload_file is None:
-    st.write("please upload csv file")
-
+if upload_file is not None:
+    # Read CSV
+    data = pd.read_csv(upload_file)
+    # Show table
+    st.write(data)
 else:
-    data - pd.read_sv(Upload_file)
-    print(data)
-
-    col1,col2,col3 = st.columns(3)
-    with col1:
-        st.metric("sales", "1,500","12%")
-    with col2:
-        st.metric("revenue", "3,000","20%")
-    with col3:
-        st.metric("user", "100","-12%")
-    st.line_chart(data['sales'])    
-    
-    if st.button("show table"):
-        st.header("sales table")
-        st.subheader("this is a sales table")
-        st.write(data)        
+    st.info("Please upload a CSV file.")
